a2a916540e625629d5d17d5e0e6f1f070e69d299
Haziq Ali <60395311+theHaziqali@users.noreply.github.com>
2024-05-17 03:05:41 +0500

    Update config.js

--- dynamo-crud-app/src/config.js
+++ dynamo-crud-app/src/config.js
@@ -2,7 +2,7 @@
 export default {
   awsConfig: {
     region: process.env.REACT_APP_region,
-    accessKeyId: process.env.REACT_APP_accessKeyId',
+    accessKeyId: process.env.REACT_APP_accessKeyId,
     secretAccessKey: process.env.REACT_APP_secretAccessKey,
   },
   tableName: 'Employee',
